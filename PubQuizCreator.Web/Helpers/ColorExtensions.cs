b5b412e6dfebd9e31acf07acd21bbcb716f6b111
budul <MarekGrochowski@web.de>
2026-04-06 02:58:10 +0200

    Refactor: split Extensions.cs into Color and JS helpers
    Separated color and JS interop extension methods into distinct files: ColorExtensions.cs for color logic (TextColor) and JsExtensions.cs for JS interop (AlertAsync, ConfirmAsync, ConfirmDeleteAsync). Removed the original Extensions.cs for improved code organization and clarity.

---
+++ PubQuizCreator.Web/Helpers/ColorExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace PubQuizCreator.Web.Helpers
+{
+    public static class ColorExtensions
+    {
+        #region Public Methods
+
+        public static string TextColor(this string hexColor)
+        {
+            var hex = hexColor.TrimStart('#');
+            if (hex.Length != 6) return "#000";
+
+            var r = Convert.ToInt32(hex[..2], 16);
+            var g = Convert.ToInt32(hex[2..4], 16);
+            var b = Convert.ToInt32(hex[4..6], 16);
+            var luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+            return luminance < 128 ? "#fff" : "#000";
+        }
+
+        #endregion Public Methods
+    }
+}
--- PubQuizCreator.Web/Helpers/JsExtensions.cs
+++ PubQuizCreator.Web/Helpers/JsExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace PubQuizCreator.Web.Helpers
 {
-    public static class ColorHelper
+    public static class JsExtensions
     {
         #region Public Methods
 
@@ -14,19 +14,6 @@ namespace PubQuizCreator.Web.Helpers
 
         public static Task<bool> ConfirmDeleteAsync(this IJSRuntime js, string name) => js
             .InvokeAsync<bool>("confirm", $"Delete \"{name}\"? This cannot be undone.").AsTask();
-
-        public static string TextColor(this string hexColor)
-        {
-            var hex = hexColor.TrimStart('#');
-            if (hex.Length != 6) return "#000";
-
-            var r = Convert.ToInt32(hex[..2], 16);
-            var g = Convert.ToInt32(hex[2..4], 16);
-            var b = Convert.ToInt32(hex[4..6], 16);
-            var luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-
-            return luminance < 128 ? "#fff" : "#000";
-        }
 
         #endregion Public Methods
     }
