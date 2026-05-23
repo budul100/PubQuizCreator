5aec32e8d8f2f102abcfa71b5182473ca95b9dd7
budul <MarekGrochowski@web.de>
2026-05-23 19:01:19 -0400

    Refactor reuse logic into ReuseButton component
    The dropdown in Index.razor was enhanced with new options, including "All categories" and "— Unusable —". The category list is now dynamically generated using a `foreach` loop.
    
    Introduced a new `ReuseButton` component to manage the reuse functionality of questions. This replaces the inline button logic in Index.razor, improving code modularity and readability.
    
    The `ReuseButton` component was implemented in ReuseButton.razor and ReuseButton.razor.cs. It handles the display and behavior of the reuse button, including dynamic styling and tooltips based on the `AllowReuse` state. It accepts parameters such as `AllowReuse`, `IsInCompletedQuiz`, `IsUnusable`, and `WasUsed`, and triggers an `EventCallback<bool>` on toggle.
    
    This refactor improves maintainability and reusability of the codebase.

---
+++ PubQuizCreator.Web/Shared/ReuseButton.razor.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components;
+
+namespace PubQuizCreator.Web.Shared
+{
+    public partial class ReuseButton
+    {
+        #region Public Properties
+
+        [Parameter] public bool AllowReuse { get; set; }
+
+        [Parameter] public bool IsInCompletedQuiz { get; set; }
+
+        [Parameter] public bool IsUnusable { get; set; }
+
+        [Parameter] public EventCallback<bool> OnToggle { get; set; }
+
+        [Parameter] public bool WasUsed { get; set; }
+
+        #endregion Public Properties
+    }
+}
